daf30bf8f5bdef575dab65d03256c3f6ed9a13ac
hmasood21955 <hmasood878@gmail.com>
2023-10-22 00:04:36 +0500

    nested

--- src/multitable.c
+++ src/multitable.c
@@ -4,3 +4,13 @@ int main() {
     int n;
     printf("Enter the value of n: ");
     scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            /* code */
+        }
+        
+    }
+    
+}
